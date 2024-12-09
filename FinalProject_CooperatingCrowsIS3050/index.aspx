cabfbb97c03931dc9d0a1137de706254c893ee2b
Andrea Gauthier <gauthiaj@mail.uc.edu>
2024-12-09 11:49:48 -0500

    Add project files.

---
+++ FinalProject_CooperatingCrowsIS3050/index.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="FinalProject_CooperatingCrowsIS3050.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Final Project Cooperating Crows</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+         <asp:Label ID="lblTitle" runat="server" Text="Welcome to the Cooperating Crows Final Project!"></asp:Label> <br />
+         <asp:Label ID="lblparagraph1" runat="server"
+            Text="Problem 4, Median of Two Arrays. Given two sorted arrays nums 1 and nums 2 of size m and n respectively, return the median of the two sorted arrays.">
+        </asp:Label>
+        <br /><br />
+
+        <asp:Label ID="lblArray1" runat="server" Text="Enter Array 1 (comma-separated):"></asp:Label>
+        <asp:TextBox ID="txtArray1" runat="server"></asp:TextBox><br /><br />
+
+        <asp:Label ID="lblArray2" runat="server" Text="Enter Array 2 (comma-separated):"></asp:Label>
+        <asp:TextBox ID="txtArray2" runat="server"></asp:TextBox><br /><br />
+
+        <asp:Button ID="bnSolveMedianOfTwoArrays" runat="server" Text="Solve" OnClick="bnSolveMedianOfTwoArrays_Click" />
+        <br /><br />
+
+        <asp:Label ID="lblSolveMedianOfTwoArraysSolution" runat="server" Font-Bold="true" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ FinalProject_CooperatingCrowsIS3050/index.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FinalProject_CooperatingCrowsIS3050
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void bnSolveMedianOfTwoArrays_Click(object sender, EventArgs e)
+        {
+                try
+                {
+                    // Convert the input strings into integer arrays
+                    int[] nums1 = Array.ConvertAll(txtArray1.Text.Split(','), int.Parse);
+                    int[] nums2 = Array.ConvertAll(txtArray2.Text.Split(','), int.Parse);
+
+                    // Call the median method
+                    double result = FindMedianSortedArrays(nums1, nums2);
+
+                    // Display the result
+                    lblSolveMedianOfTwoArraysSolution.Text = $"The median is: {result}";
+                }
+                catch (Exception)
+                {
+                    lblSolveMedianOfTwoArraysSolution.Text = "Error: Please enter valid integers separated by commas.";
+                }
+            }
+
+            // Function to find the median of two sorted arrays
+            public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+            {
+                if (nums1.Length > nums2.Length)
+                {
+                    return FindMedianSortedArrays(nums2, nums1);
+                }
+
+                int m = nums1.Length, n = nums2.Length;
+                int low = 0, high = m;
+
+                while (low <= high)
+                {
+                    int partitionX = (low + high) / 2;
+                    int partitionY = (m + n + 1) / 2 - partitionX;
+
+                    int maxX = (partitionX == 0) ? int.MinValue : nums1[partitionX - 1];
+                    int minX = (partitionX == m) ? int.MaxValue : nums1[partitionX];
+
+                    int maxY = (partitionY == 0) ? int.MinValue : nums2[partitionY - 1];
+                    int minY = (partitionY == n) ? int.MaxValue : nums2[partitionY];
+
+                    if (maxX <= minY && maxY <= minX)
+                    {
+                        if ((m + n) % 2 == 0)
+                        {
+                            return (Math.Max(maxX, maxY) + Math.Min(minX, minY)) / 2.0;
+                        }
+                        else
+                        {
+                            return Math.Max(maxX, maxY);
+                        }
+                    }
+                    else if (maxX < minY)
+                    {
+                        low = partitionX + 1;
+                    }
+                    else
+                    {
+                        high = partitionX - 1;
+                    }
+                }
+
+                throw new ArgumentException("The input arrays are not sorted.");
+            }
+        }
+    }
